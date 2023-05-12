4b7bd6c2518bbf9be67795aa0d7cce9e3f255cd9
isthifa <4pa19cs404@pace.edu.in>
2023-05-12 14:57:24 +0530

    logging and swagger api

--- src/main/java/com/example/spring_exception_validation/SpringExceptionValidationApplication.java
+++ src/main/java/com/example/spring_exception_validation/SpringExceptionValidationApplication.java
@@ -1,9 +1,12 @@
 package com.example.spring_exception_validation;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger API Demo", version = "1.2", description = "crud operation"))
 public class SpringExceptionValidationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/spring_exception_validation/controller/PersonController.java
+++ src/main/java/com/example/spring_exception_validation/controller/PersonController.java
@@ -2,16 +2,25 @@ package com.example.spring_exception_validation.controller;
 
 import com.example.spring_exception_validation.entity.Person;
 import com.example.spring_exception_validation.service.PersonService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import jakarta.validation.Valid;
 import jakarta.validation.executable.ValidateOnExecution;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/person")
 public class PersonController {
+    Logger logger= LoggerFactory.getLogger(PersonController.class);
     private PersonService personService;
     @Autowired
     public PersonController(PersonService personService)
@@ -19,34 +28,44 @@ public class PersonController {
         this.personService=personService;
     }
     @PostMapping("/save")
+    @Operation(summary = "Save data",description = "Save Operation")
     public Person save(@Valid @RequestBody Person person)
     {
         Person person1=personService.save(person);
+        logger.info("Saving the data");
         return person;
     }
     @GetMapping("/list")
+    @Operation(summary = "list all data")
     public List<Person> list()
     {
         List<Person> personList=personService.listall();
+        //logger.trace("list method accessed");
+        logger.info("return list of personlist");
         return personList;
     }
     @GetMapping("/{id}")
+    @Operation(summary = "find by id")
     public Person getbyid(@Valid @PathVariable long id)
     {
         Person person=personService.findbyid(id);
+        logger.info("find by id");
         return person;
     }
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "delete by id", description = "id should not be null")
     public String Delete(@PathVariable long id)
     {
         Person person=personService.findbyid(id);
         if(person!=null) {
+            logger.info("deleting the data");
             personService.deletebyid(id);
         }
         return "Deleted "+id;
     }
 
     @PutMapping("/update/{id}")
+    @Operation(summary = "Update the existing data",description ="value min 2 character, should not be null" )
     public Person update(@Valid @PathVariable long id,@RequestBody Person person)
     {
         Person person1=personService.findbyid(id);
@@ -54,6 +73,7 @@ public class PersonController {
         person1.setFirstname(person.getFirstname());
         person1.setLastname(person.getLastname());
         person1.setEmail(person.getEmail());
+        logger.info("updating");
         personService.save(person1);
 
         return person1;
